efd47f8cd4ba31a00097ff4e4bda68714421b1a5
Thiago Varjão <tvarjao@gmail.com>
2020-03-28 15:14:08 -0300

    Frontend telas logon e cadastro

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import './style.css';
+import logoImg from '../../assets/logo.svg';
+
+
+export default function Register(){
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Logo"/>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro Lorem Ipsum</p>
+                    <Link className="link-register" to="/">
+                        <FiArrowLeft size={16} color="#e02041"/>
+                        Voltar
+                    </Link>
+                </section>
+                <form>
+                    <input placeholder="Nome da ONG"/>
+                    <input type="email" placeholder="E-mail"/>
+                    <input placeholder="WhatsApp"/>
+                    <div className="input-group">
+                        <input placeholder="Cidade"/>
+                        <input placeholder="UF" style={{width: 80}}/>
+                    </div>
+                    <button className="button">Cadastrar</button>
+                </form>
+
+            </div>
+        </div>
+    );
+}
